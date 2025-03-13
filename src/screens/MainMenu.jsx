93f23068712e2e5b15628c543fb5ac55a911a5de
Thomas Eikhaugen <Thomas.Eikhaugen@gmail.com>
2025-03-13 17:26:43 +0100

    added screens for all views of game

---
+++ src/screens/MainMenu.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { useGameStore } from '../store/useGameStore';
+
+export default function MainMenu() {
+  const setCurrentView = useGameStore((state) => state.setCurrentView);
+
+  const goToSplash = () => setCurrentView('SPLASH');
+  const goToMainMenu = () => setCurrentView('MAIN_MENU');
+  const goToMap = () => setCurrentView('MAP');
+  const goToBattle = () => setCurrentView('BATTLE');
+  const goToCharacterSheet = () => setCurrentView('CHARACTER_SHEET');
+  const goToShop = () => setCurrentView('SHOP');
+
+  return (
+    <div className="space-x-2 mt-4">
+      <h1>Main Menu</h1>
+      <button
+        className="px-2 py-1 border rounded bg-gray-700"
+        onClick={goToSplash}
+      >
+        Splash
+      </button>
+      <button
+        className="px-2 py-1 border rounded bg-gray-700"
+        onClick={goToMainMenu}
+      >
+        Main Menu
+      </button>
+      <button
+        className="px-2 py-1 border rounded bg-gray-700"
+        onClick={goToMap}
+      >
+        Map
+      </button>
+      <button
+        className="px-2 py-1 border rounded bg-gray-700"
+        onClick={goToBattle}
+      >
+        Battle
+      </button>
+      <button
+        className="px-2 py-1 border rounded bg-gray-700"
+        onClick={goToCharacterSheet}
+      >
+        Character
+      </button>
+      <button
+        className="px-2 py-1 border rounded bg-gray-700"
+        onClick={goToShop}
+      >
+        Shop
+      </button>
+    </div>
+  );
+}
