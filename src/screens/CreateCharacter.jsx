2280c388f29d3c9ddab569a288ae0b814b8e0e1a
Thomas Eikhaugen <Thomas.Eikhaugen@gmail.com>
2025-03-14 16:06:34 +0100

    Added CreateCharacter screen

---
+++ src/screens/CreateCharacter.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import { useGameStore } from '../store/useGameStore';
+
+export default function CreateCharacter() {
+  const setCurrentView = useGameStore((state) => state.setCurrentView);
+
+  const goToSplash = () => setCurrentView('SPLASH');
+  const goToMainMenu = () => setCurrentView('MAIN_MENU');
+  const goToMap = () => setCurrentView('MAP');
+  const goToBattle = () => setCurrentView('BATTLE');
+  const goToCharacterSheet = () => setCurrentView('CHARACTER_SHEET');
+  const goToShop = () => setCurrentView('SHOP');
+  const goToCreateCharacter = () => setCurrentView('CREATE_CHARACTER');
+
+  return (
+    <div className="space-x-2 mt-4">
+        <h1>CharacterSheet</h1>
+      <button
+        className="px-2 py-1 border rounded bg-gray-700"
+        onClick={goToSplash}
+      >
+        Splash
+      </button>
+      <button
+        className="px-2 py-1 border rounded bg-gray-700"
+        onClick={goToMainMenu}
+      >
+        Main Menu
+      </button>
+      <button
+        className="px-2 py-1 border rounded bg-gray-700"
+        onClick={goToMap}
+      >
+        Map
+      </button>
+      <button
+        className="px-2 py-1 border rounded bg-gray-700"
+        onClick={goToBattle}
+      >
+        Battle
+      </button>
+      <button
+        className="px-2 py-1 border rounded bg-gray-700"
+        onClick={goToCharacterSheet}
+      >
+        Character
+      </button>
+      <button
+        className="px-2 py-1 border rounded bg-gray-700"
+        onClick={goToShop}
+      >
+        Shop
+      </button>
+      <button
+        className="px-2 py-1 border rounded bg-gray-700"
+        onClick={goToCreateCharacter}
+      >
+        Create Character
+      </button>
+    </div>
+  );
+}
