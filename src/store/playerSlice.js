d3070c1d293a1768f93c6b3260e2a1b33c8d4864
Thomas Eikhaugen <Thomas.Eikhaugen@gmail.com>
2025-03-14 15:45:07 +0100

    added playerSlice.js file

---
+++ src/store/playerSlice.js
@@ -0,0 +1,12 @@
+export const createPlayerSlice = (set, get) => ({
+    player: {
+      name: 'Hero',
+      level: 1,
+      currentHp: 100,
+      maxHp: 100,
+      attack: 10,
+      defense: 5,
+      xp: 0,
+    },
+  });
+  
