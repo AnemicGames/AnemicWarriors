662c98f5e692d5dce443dd0e1860db5c8edc7958
Thomas Eikhaugen <Thomas.Eikhaugen@gmail.com>
2025-03-14 15:42:26 +0100

    Added embark state and comments to embark and currentview state

--- src/store/useGameStore.js
+++ src/store/useGameStore.js
@@ -5,11 +5,19 @@ export const useGameStore = create(
   persist(
     (set, get) => ({
 
-      currentView: 'SPLASH', 
+      //currentView brukes for å vite hvilken view vi skal vise i appen.
+      currentView: 'SPLASH',
       setCurrentView: (view) => set({ currentView: view }),
+
+      //Embark state bruker vi for å vite vi skal navigere til main menu eller map fra shop.
+      //Når embark er false når shoppen lukke skal vi navigere til main menu, hvis den er true skal vi navigere til map.
+      //Embark settes til true når vi trykker på embark knappen i Main Menu.
+      //Embark settes til false når vi har drept bossen eller dør i kamp og blir sendt tilbake til main menu.
+      embark: false,
+      setEmbark: (value) => set({ embark: value }),
     }),
     {
-      name: 'anemic-heroes-store', 
+      name: 'anemic-heroes-store',
     }
   )
 );
